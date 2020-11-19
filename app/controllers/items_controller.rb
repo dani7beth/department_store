32b5482e4ef81ec732f45233085c8b2c813d3e1b
dani7beth <danielledona28@gmail.com>
2020-11-18 17:40:06 -0700

    finished crud for items model

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,55 @@
 class ItemsController < ApplicationController
+  before_action :set_department
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   def index
+    @items = @department.items
   end
 
   def show
   end
 
   def new
+    @item = @department.items.new
+  end
+
+  def create
+    @item = @department.items.new(item_params)
+
+    if @item.save
+      # this up to you as dev where to go
+      redirect_to department_item_path(@department, @item)
+    else
+      # want to go back form if error
+      render :new
+    end
   end
 
   def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to department_item_path(@department, @item)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to department_items_path(@department)
+  end
+
+  private
+  def set_department
+    @department = Department.find(params[:department_id])
+  end
+
+  def set_item
+    @item = @department.items.find(params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :price)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Item.destroy_all
+
+#for games department add some random games
+5.times do
+    Department.find(1).items.create(name: Faker::Game.title, price: rand(11.30...59.99).round(2))
+end
+
+#for Comic Books department add some random comics
+5.times do
+    Department.find(2).items.create(name: Faker::DcComics.title, price: rand(5.00...100.99).round(2))
+end
+
+#for snacks department add some random snacks
+5.times do
+    Department.find(3).items.create(name: Faker::Food.fruits, price: rand(1.00...6.99).round(2))
+end
+
